c336f473aef9785a2f72e480f12231b258699da9
Ajinkya Dharashive <adharashive@gmail.com>
2019-03-20 11:34:40 +0530

    Update Ui.py

--- Ui.py
+++ Ui.py
@@ -1,6 +1,8 @@
 import tkinter
 from tkinter import *
-
+import tkinter as tk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 root = tkinter.Tk()
 root.attributes('-fullscreen', True)
@@ -10,49 +12,57 @@ var1 = StringVar()
 w = Label(root, textvariable=var1,font=("Helvetica", 16)).pack()
 var1.set("GeoPi Tracker")
 
-def generate_ui(ol,olo,rl,rlo,d,status):
+
+def generate_ui(status,rl,rlo,d):
+    def our_coordnates():
+        print(var5.get())
+        print(var6.get())
+        return
+
     #
     w = Label(root, text="Status : ",font=("Helvetica", 10))
     w.place(x = 200, y = 100 , width=120, height=25)
     var2 = StringVar()
-    w = Label(root, textvariable=var2,font=("Helvetica", 10),bg="white")
-    w.place(x =300 , y = 100 , width=120, height=25)
+    w1 = Label(root, textvariable=var2,font=("Helvetica", 10),bg="white")
+    w1.place(x =300 , y = 100 , width=120, height=25)
     var2.set(status)
     #
     s = Label(root, text="Our Latitude : ",font=("Helvetica", 10))
     s.place(x = 200, y = 175 , width=120, height=25)
     var5 = StringVar()
-    s = Entry(root, textvariable=var5,font=("Helvetica", 10),bg="white")
-    s.place(x =350 , y = 175 , width=120, height=25)
-    var5.set(ol)
+    s1 = Entry(root, textvariable=var5,font=("Helvetica", 10),bg="white")
+    s1.place(x =350 , y = 175 , width=120, height=25)
     #
     t = Label(root, text="Our Longitude : ",font=("Helvetica", 10))
     t.place(x = 600, y = 175 , width=120, height=25)
     var6 = StringVar()
-    t = Entry(root, textvariable=var6,font=("Helvetica", 10),bg="white")
-    t.place(x =750 , y = 175 , width=120, height=25)
-    var6.set(olo)
+    t1 = Entry(root, textvariable=var6,font=("Helvetica", 10),bg="white")
+    t1.place(x = 750 , y = 175 , width=120, height=25)
+    #
+    b = tk.Button(root, text="Submit", command=our_coordnates).pack()
+    #b.place(x = 900, y = 175)
     #
     x = Label(root, text="Remote Latitude : ",font=("Helvetica", 10))
     x.place(x = 200, y = 250 , width=120, height=25)
     var3 = StringVar()
-    x = Label(root, textvariable=var3,font=("Helvetica", 10),bg="white")
-    x.place(x =350 , y = 250 , width=120, height=25)
+    x1 = Label(root, textvariable=var3,font=("Helvetica", 10),bg="white")
+    x1.place(x =350 , y = 250 , width=120, height=25)
     var3.set(rl)
     #
     y = Label(root, text="Remote Longitude : ",font=("Helvetica", 10))
     y.place(x = 600, y = 250 , width=120, height=25)
     var4 = StringVar()
-    y = Label(root, textvariable=var4,font=("Helvetica", 10),bg="white")
-    y.place(x =750 , y = 250, width=120, height=25)
+    y1 = Label(root, textvariable=var4,font=("Helvetica", 10),bg="white")
+    y1.place(x =750 , y = 250, width=120, height=25)
     var4.set(rlo)
+    #
+    z = Label(root, text="Distance : ", font=("Helvetica", 10))
+    z.place(x=900, y=250, width=120, height=25)
+    var7 = StringVar()
+    z1 = Label(root, textvariable=var4, font=("Helvetica", 10), bg="white")
+    z1.place(x=1020, y=250, width=120, height=25)
+    var7.set(d)
 
-generate_ui(10,20,30,40,50,60)
 
-#graphFrame = LabelFrame(root, height = 500, width = 800, text = "Trajectory Covered")
-#graphFrame.place(x=350,y=300)
-
-
-
-
+generate_ui(30,40,50,60)
 root.mainloop()
